c36d6d2114fd84038dd357253d4de056648ef08f
unknown <marycamilainfo@gmail.com>
2018-05-16 20:21:05 -0300

    Alteração da DAO

---
+++ ProjetoMVC/ProjetoClinica/ProjetoClinica/src/com/up/clinica/model/dao/TipoAnimalDAO.java
@@ -0,0 +1,38 @@
+package com.up.clinica.model.dao;
+
+import java.util.List;
+
+import com.up.clinica.model.TipoAnimal;
+
+public class TipoAnimalDAO implements IGenericDAO<TipoAnimal, String>{
+
+	@Override
+	public List<TipoAnimal> listar() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoAnimal buscar(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remover(String id) throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void atualizar(TipoAnimal objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void persistir(TipoAnimal objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
